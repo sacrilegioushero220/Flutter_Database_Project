46e73b3ad43337728639af83977a307f19f1e916
sacrilegioushero220 <sandweepsabu@gmail.com>
2023-07-19 00:18:15 +0530

    UI part is completed

--- lib/screenhome.dart
+++ lib/screenhome.dart
@@ -10,19 +10,56 @@ class ScreenHome extends StatelessWidget {
         title: const Text('Hive Demo Project'),
         elevation: 2,
       ),
-      body: const Column(
-        children: [AddStudentWidget(), Expanded(child: ListStudentNames())],
+      body: Column(
+        children: [
+          AddStudentWidget(),
+          const Expanded(child: ListStudentNames())
+        ],
       ),
     );
   }
 }
 
 class AddStudentWidget extends StatelessWidget {
-  const AddStudentWidget({super.key});
+  AddStudentWidget({super.key});
+  final _nameController = TextEditingController();
+  final _ageController = TextEditingController();
+
+  onAddStudentButtonClicked() {
+    final name = _nameController.text.trim();
+    final age = _ageController.text.trim();
+    if (name.isEmpty || age.isEmpty) {
+      return;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(children: [
+        TextFormField(
+          controller: _nameController,
+          decoration: const InputDecoration(
+            labelText: 'Student Name',
+            border: OutlineInputBorder(),
+          ),
+        ),
+        const SizedBox(height: 10),
+        TextFormField(
+          controller: _ageController,
+          decoration: const InputDecoration(
+            labelText: 'Student Age',
+            border: OutlineInputBorder(),
+          ),
+        ),
+        const SizedBox(height: 10),
+        ElevatedButton.icon(
+            onPressed: onAddStudentButtonClicked(),
+            icon: const Icon(Icons.add),
+            label: const Text('Add Student'))
+      ]),
+    );
   }
 }
 
@@ -31,6 +68,16 @@ class ListStudentNames extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView.separated(
+        itemBuilder: (ctx, index) {
+          return ListTile(
+            title: Text('Student Name $index'),
+            subtitle: Text('Student Age $index'),
+          );
+        },
+        separatorBuilder: (ctx, index) {
+          return const Divider();
+        },
+        itemCount: 50);
   }
 }
